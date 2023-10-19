f8d1fb7b808810c183679f43c6e0cb67d613c242
elafha <146969215+elafha@users.noreply.github.com>
2023-10-19 16:44:16 +0300

    Add files via upload

---
+++ mybackend/mybackendapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MybackendappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mybackendapp'
---
+++ mybackend/mybackendapp/views.py
@@ -0,0 +1,8 @@
+# from django.shortcuts import render
+from requests import Response
+from rest_framework.views import View
+from rest_framework.response import Response
+
+from .models import User
+
+# Create your views here
