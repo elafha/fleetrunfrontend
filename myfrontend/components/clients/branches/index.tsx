c3347ca04ea23ac0d98169a8a90791d1499fed86
ZineddineBk09 <eino1209@gmail.com>
2023-11-04 10:05:55 +0100

    use branches context + scroll to selected branch

--- myfrontend/components/clients/branches/index.tsx
+++ myfrontend/components/clients/branches/index.tsx
@@ -4,12 +4,24 @@ import { AddBranch } from './add-branch'
 import { SearchBranch } from './branch'
 import dynamic from 'next/dynamic'
 const BranchCard = dynamic(() => import('./branch'), { ssr: false })
-import { faker } from '@faker-js/faker'
 import { useClientsBranchesContext } from '@/context/clients/BranchesContext'
+import { useRouter } from 'next/router'
 
 const Branches = () => {
   const { branches } = useClientsBranchesContext()
-
+  // get the ?branchId= query param
+  const router = useRouter()
+  const { branchId } = router.query
+  
+  
+  // if branches contains the branchId, then scroll to it
+  React.useEffect(() => {
+    if (branchId && branches.some((branch: Branch) => branch.id === branchId)) {
+      const branchElement = document.getElementById(branchId as string)
+      branchElement?.scrollIntoView({ behavior: 'smooth' })
+    }
+  }, [branchId, branches])
+  
   return (
     <div className='w-full mx-auto flex flex-col items-center gap-y-6'>
       <SearchBranch />
