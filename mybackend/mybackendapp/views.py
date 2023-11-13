ed72f2529bc503bd65347d5fcfdcd469942571ae
ZineddineBk09 <eino1209@gmail.com>
2023-11-13 11:21:46 +0100

    remove backend folder

--- mybackend/mybackendapp/views.py
+++
@@ -1,8 +0,0 @@
-# from django.shortcuts import render
-from requests import Response
-from rest_framework.views import View
-from rest_framework.response import Response
-
-from .models import User
-
-# Create your views here
